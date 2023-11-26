86c60413d91d3d5bd0baf3a5ba4016c0d1764107
Jonathan Tan <jtsz5057@gmail.com>
2023-11-26 18:52:22 +1000

    create http route for the app

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import { PORT } from "./config.js"
 
 const app = express()
 
+//http route
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to my jonathan page')
+});
+
 app.listen(PORT, () => {
     console.log(`App is listning to: ${PORT}`)
 })
