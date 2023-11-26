ab8a94fd425cca9f57192b71f5503ce3ac25112a
Jonathan Tan <jtsz5057@gmail.com>
2023-11-26 19:23:29 +1000

    add mongoDB and mongosse to nodejs and connect to application

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
 export const PORT = 5555;
+
+// connection string to mongodb
+export const mongoDBURL = 
+    'mongodb+srv://jtsz5057:910112835057@cluster0.uocmbda.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose";
 
 const app = express()
 
@@ -12,3 +13,12 @@ app.get('/', (request, response) => {
 app.listen(PORT, () => {
     console.log(`App is listning to: ${PORT}`)
 })
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=> {
+        console.log('App connected to database');
+    })
+    .catch((error) => {
+        console.log(error);
+    })
