e3fa16844258a50d30ef719e692890c857620fc8
Jonathan Tan <jtsz5057@gmail.com>
2023-12-03 22:32:55 +1000

    create cors policy in our app server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 import express, { request, response } from "express";
-import { PORT, mongoDBURL } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js"
-import booksRoute from "./routes/booksRoute.js"
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express()
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS Policy
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 // http route
 app.get('/', (request, response) => {
