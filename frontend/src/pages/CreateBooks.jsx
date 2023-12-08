37b02ddd75debe377016c9d31401b8ac987b88e0
Jonathan Tan <jtsz5057@gmail.com>
2023-12-09 08:38:24 +1000

    create the page for creating new book

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState('');
+  const navigate = useNavigate();
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books')
+  }
+
+
   return (
     <div>CreateBooks</div>
   )
