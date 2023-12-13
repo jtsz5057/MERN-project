cd4a1caef630a57850a6129b993f9e4e0d1d8f10
Jonathan Tan <jtsz5057@gmail.com>
2023-12-13 17:39:20 +1000

    continue creating the UI in showing the books list oncards sequence and table sequence, refractoring the code.

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
-const BooksTable = () => {
+const BooksTable = ( {books} ) => {
   return (
     <table className='w-full border-separate border-spacing-2'>
     <thead>
@@ -17,7 +21,35 @@ const BooksTable = () => {
       </tr>
     </thead>
     <tbody>
-
+      {books.map((book, index) => (
+        <tr key={book._id} className='h-8'>
+          <td className='border border-slate-700 rounded=md text-center'>
+            {index + 1}
+          </td>
+          <td className='border border-slate-700 rounded-md text-center'>
+            {book.title}
+          </td>
+          <td className='Border border-slate-700 rounded-md text-center max-md:hidden'>
+            {book.author}
+          </td>
+          <td className='Border border-slate-700 rounded-md text-center max-md:hidden'>
+            {book.publishYear}
+          </td>
+          <td className='border border-slate-700 rounded-md text-center'>
+            <div className="flex justify-center">
+              <Link to={`/books/details/${book._id}`}>
+                <BsInfoCircle className='text-2xl text-green-800' />
+              </Link>
+              <Link to={`/books/edit/${book._id}`}>
+                <AiOutlineEdit className='text-2xl text-yellow-600' />
+              </Link>
+              <Link to={`/books/delete/${book._id}`}>
+                <MdOutlineAddBox className='text-2xl text-red-600' />
+              </Link>
+            </div>
+          </td>
+        </tr>
+      ))}
     </tbody>
   </table>
   )
